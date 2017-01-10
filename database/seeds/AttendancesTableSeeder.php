54ea8350ae84353d8dcd3548b920a82844bedaba
uenoryo <r.r.ag0611@outlook.jp>
2017-01-10 17:56:03 +0900

    Fix spelling

--- database/seeds/AttendancesTableSeeder.php
+++ database/seeds/AttendancesTableSeeder.php
@@ -18,17 +18,17 @@ class AttendancesTableSeeder extends Seeder
                 'id'         => 1,
                 'lecture_id' => 1,
                 'student_id' => 18114035,
-                'is_acctive' => 1,
+                'is_active' => 1,
             ],            [
                 'id'         => 2,
                 'lecture_id' => 1,
                 'student_id' => 18114034,
-                'is_acctive' => 1,
+                'is_active' => 1,
             ],            [
                 'id'         => 3,
                 'lecture_id' => 1,
                 'student_id' => 18114033,
-                'is_acctive' => 0,
+                'is_active' => 0,
             ],
         ];
 
